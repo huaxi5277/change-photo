358cc742df353ae5667bffff307a5e00748ba7a5
huaxi527 <xue19970326@qq.com>
2019-09-28 16:39:37 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import image1 from './img/小美.jpg';
+import image2 from './img/roly.jpg';
+import image3 from './img/timg.jpg';
 import './App.css';
+import PropTypes from 'prop-types'
+var arrImage = [image1,image2,image3];
+class ImgWrap extends Component{
+  static contextTypes = {
+    index : PropTypes.number
+  }
+  constructor(props){
+    super(props)
+  }
+  render(){
+    console.log(this.context)
+    return (
+      <div className="wrap-img">
+       <ul className="ul-list">
+       {
+          arrImage.map((item,index)=>{
+            
+              return (
+                <li key={index} className={`list-item ${index === this.context.index ? "active" : ""}`}>
+                  <img src={item} alt=""/>
+                </li>
+              )
+          })
+        }
+       </ul>
+      </div>
+    )
+  }
+}
+class Button extends Component{
+  static contextTypes = {
+    change : PropTypes.func,
+    index : PropTypes.number
+  }
+  constructor(){
+    super()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  render(){
+    console.log(this.context)
+    return (
+      <div className="button-group">
+        <button className="left btn" onClick={()=>this.context.change(this.context.index)}>{"<"}</button>
+        <button className="right btn" onClick={()=>this.context.change(this.context.index)}>{">"}</button>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  static childContextTypes = {
+     index : PropTypes.number,
+     change : PropTypes.func
+  }
+  getChildContext(){
+    return {
+      index : this.state.index,
+      change : (index)=>{this._goChange(index)}
+    }
+  }
+ constructor(){
+   super()
+   this.state ={
+     index : 0
+   }
+ }
+ _goChange(index){
+   console.log(index)
+   if(index == arrImage.length-1){
+     index = 0;
+   }
+   else {
+     index ++
+   }
+this.setState({
+index : index
+})
+ }
+ render(){
+   return (
+     <div className="wrap">
+     <ImgWrap ></ImgWrap>
+     <Button ></Button>
+     </div>
+   )
+ }
 }
 
 export default App;
